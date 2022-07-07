69d61b2be3a5499296b056b4040f9ce3891e05dd
EVGENIY-12 <93032109+EvgeniyArabadzhiyskiy@users.noreply.github.com>
2022-07-07 14:15:06 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
 
 import Container from 'components/Container/Container';
 import Section from 'components/Feedback/Section/Section';
@@ -6,39 +6,26 @@ import Notification from 'components/Feedback/Notification/Notification';
 import Statistics from 'components/Feedback/Statistics/Statistics';
 import FeedbackOptions from 'components/Feedback/FeedbackOptions/FeedbackOptions';
 
-let state = {};
+function feedbackReducer(state, { type, payload }) {
+  return {
+    ...state,
+    [type]: state[type] + payload,
+  };
+}
 
 const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-
-  state = {
-    good,
-    neutral,
-    bad,
-  };
+  const [feedbackTypes, dispatch] = useReducer(feedbackReducer, {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
 
   const onFeedbackBtnClick = feedback => {
-    switch (feedback) {
-      case 'good':
-        setGood(prevState => prevState + 1);
-        break;
-
-      case 'neutral':
-        setNeutral(prevState => prevState + 1);
-        break;
-      case 'bad':
-        setBad(prevState => prevState + 1);
-        break;
-
-      default:
-        return;
-    }
+    dispatch({ type: feedback, payload: 1 });
   };
 
   const getStateEntries = () => {
-    return Object.entries(state);
+    return Object.entries(feedbackTypes);
   };
 
   const countTotalFeedback = () => {
@@ -46,14 +33,17 @@ const App = () => {
   };
 
   const countPositiveFeedbackPercentage = () => {
-    return Math.round(100 / (countTotalFeedback() / state.good));
+    return Math.round(100 / (countTotalFeedback() / feedbackTypes.good));
   };
 
   return (
     <div>
       <Container>
         <Section title="Please leave feedback" />
-        <FeedbackOptions options={state} onLeaveFeedback={onFeedbackBtnClick} />
+        <FeedbackOptions
+          options={feedbackTypes}
+          onLeaveFeedback={onFeedbackBtnClick}
+        />
 
         <div>
           <Section title="Statistic" />
